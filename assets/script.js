7af11fceafd441f296bd39f2f0429d564c8e9032
Mariannereissss <dmariannereis@gmail.com>
2025-08-07 22:21:53 -0300

    segundo commit

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//carrossel 1//
+document.addEventListener("DOMContentLoaded", () => {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".slide");
+    const anterior = document.querySelector(".antes");
+    const proximo = document.querySelector(".depois");
+
+    function mostrarSlide(index) {
+        slides.forEach(slide => slide.classList.remove("ativo"));
+        slides[index].classList.add("ativo");
+    }
+
+    anterior.addEventListener("click", () => {
+        slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+        mostrarSlide(slideIndex);
+    });
+
+    proximo.addEventListener("click", () => {
+        slideIndex = (slideIndex + 1) % slides.length;
+        mostrarSlide(slideIndex);
+    });
+});
+
+  //carrosel 2//
